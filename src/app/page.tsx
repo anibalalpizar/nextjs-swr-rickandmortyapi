bfa3da8233e68748f7e328f9ffff96ad007dace8
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-13 03:28:40 -0600

    Refactor Page component to implement character pagination and loading state with SWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,56 @@
-import Link from "next/link"
+"use client"
 
-function page() {
+import { useRouter, usePathname, useSearchParams } from "next/navigation"
+import useSWR from "swr"
+import { Button, Col, Row, Spinner } from "react-bootstrap"
+import * as RickMortyApi from "@/network/rickAndMorty.api"
+import RickMortyEntry from "@/componets/RickMortyEntry"
+
+function Page() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const handleNavigation = (newPage: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("page", newPage.toString())
+    router.push(`${pathname}?${params.toString()}`)
+  }
+
+  const page = parseInt(searchParams.get("page") || "1")
+
+  const { data, isLoading } = useSWR(["getCharactersPage", page], () =>
+    RickMortyApi.getCharactersPage(page)
+  )
+
+  if (isLoading) {
+    return <Spinner animation="border" className="d-block mx-auto" />
+  }
+
   return (
     <div>
       <h1 className="text-center mb-4">Rick and Morty</h1>
-      <Link href="/characters/1" className="link-dark">
-        Go to first page
-      </Link>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-4">
+        {data?.results.map((character) => (
+          <Col key={character.id}>
+            <RickMortyEntry id={character.id} />
+          </Col>
+        ))}
+      </Row>
+      <div className="d-flex justify-content-center gap-2 mt-4">
+        {data?.info.prev && (
+          <Button onClick={() => handleNavigation(page - 1)} variant="primary">
+            Página anterior
+          </Button>
+        )}
+        {data?.info.next && (
+          <Button onClick={() => handleNavigation(page + 1)} variant="primary">
+            Página siguiente
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page
