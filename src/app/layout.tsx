d24493302dc422f0b1fccd72c06469d73892896e
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-13 03:27:48 -0600

    Update layout to import global styles from new path and add global CSS file for consistent styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import "@/styles/globals.css"
 import { Container } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
